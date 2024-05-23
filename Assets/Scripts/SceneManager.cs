dca1a2e10fa3ecd0fb2d3a0a1931376734bf5bab
MartaGnarta <martikajover@icloud.com>
2024-05-23 18:04:45 +0200

    Revert "Merge branch 'main' into bowling"
    This reverts commit cae1af015c5db75355115db7d93ec038997fadb5, reversing
    changes made to fb195431ed6db9e2593864b6cc23302ce212c169.

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -8,7 +8,6 @@ public class MainMenu : MonoBehaviour
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
-        Time.timeScale = 1f;
     }
 
     public void QuitGame()
